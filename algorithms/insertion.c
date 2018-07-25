394e4a3b7c410052ec8a9ce2d25c6d9fef3265be
TahaMagdy <tahamagdy314@gmail.com>
2018-07-25 21:23:47 +0200

    clearner code

--- algorithms/insertion.c
+++ algorithms/insertion.c
@@ -5,41 +5,44 @@
 // * pre-condition : passing unsorted integer array.
 // * post-condition: the passed array is sorted.
 void
-insertion_sort ( int* ar, int size )
+insertion_sort(int* ar, int size)
 {
-  int table,
-      hand ,
+  int current,
+      biggestSorted,
       key  ;
-  for( table = 1; table < size; table++ )
+  for(current = 1; current < size; current++)
     {
-       key  = ar[table]; // picking the current key.
-       hand = table - 1; // the greatest sorted part element.
+        key  = ar[current]; // picking the current key.
 
-       // * Inserting the key into the sorted part.
-       // * if the key in less than its previous we
-       //   shift all the previous ones  to the
-       //   right. until key is not less than
-       //   
-       while ( ( hand >= 0 ) && ( key < ar[hand] ) )
-         {
-	   // * moving ar[hand] to the right,
-	   //   the key will be overwridde,
-	   //   it's ok, becuse we have it in the $key
-	   ar[hand+1] = ar[hand]; 
+        // Repositioning the biggestSorted flag to the largest element
+        // in the sorted part
+        biggestSorted = current - 1; // the greatest sorted part element.
 
-	   // * moving the index to the left
-	   //   to ensure that the left is not
-	   //   greater than the key
-	   hand --;
-	 } // # end inserting the element;
+        // * Inserting the key into the sorted part.
+        // * if the key in less than its previous we
+        //   shift all the previous ones  to the
+        //   right. until key is not less than
+        //   
+        while ((biggestSorted >= 0 ) && (key < ar[biggestSorted]))
+        {
+        // * moving ar[biggestSorted] to the right,
+        //   the key will be overwridde,
+        //   it's ok, becuse we have it in the $key
+        ar[biggestSorted+1] = ar[biggestSorted]; 
 
-       // * inserting the key into its position in
-       //   the sorted area.
-       // * where it is not less than its previous.
-       ar[hand+1] = key;  
-    } // # end table
-return;
-} // end insertion()
+        // * moving the index to the left
+        //   to ensure that the left is not
+        //   greater than the key
+        biggestSorted --;
+        } // # end inserting the element;
+
+        // * inserting the key into its position in
+        //   the sorted area.
+        // * where it is not less than its previous.
+        ar[biggestSorted+1] = key;  
+        } // # end current
+        return;
+    } // end insertion()
 
 			/////////////////////
 			/////////////////////
@@ -47,6 +50,7 @@ return;
 // ^^ Testing...
 int main()
 {
+  printf("Enter 5 elements; Or change the SIZE\n");
   int b [SIZE];
   int i = 0;
   while ( i < SIZE )
