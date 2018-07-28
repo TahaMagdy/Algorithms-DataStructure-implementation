4cf910256e410d0c488f6b72f24ae4e570b55026
TahaMagdy <tahamagdy314@gmail.com>
2018-07-28 14:13:19 +0200

    add append

--- data structures/LinkedList/linkedList.py
+++ data structures/LinkedList/linkedList.py
@@ -5,22 +5,37 @@
 """
 
 class Node:
-    def __init__(self, data=None):
+    def __init__(self, data):
         self.data = data
         self.next = None
 
 
 class LinkedList:
     # LinkedList definition
-    def __init(self):
-        self.head = Node()
+    def __init__(self):
+        self.head = Node(data=None)
 
     # LinkedList Operations
-    def append(self, data):
-        pass
+    def append(self, element):
+        # Getting the head
+        currentNode = self.head
+        # Moving over the next nodes
+        while currentNode.next != None:
+            currentNode = currentNode.next
+        # Appending the new element to the last node's next.
+        currentNode.next = Node(element)
 
     def getByIndex(self, index):
         pass
 
     def traverse(self, function):
         pass
+
+    def printElements(self):
+        pass
+
+"""Testing"""
+if __name__ == "__main__":
+    myList = LinkedList()
+    myList.append(12)
+    myList.append(13)
