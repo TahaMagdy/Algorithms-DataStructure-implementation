e291e0284af19b680f34b6eaddb371c34ae4a63c
TahaMagdy <tahamagdy314@gmail.com>
2018-07-28 14:03:33 +0200

    building the Node and the LinkedList

---
+++ data structures/LinkedList/linkedList.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+"""1 Creating the linked list ADT
+   2 Creating its operations
+   3 Testing
+"""
+
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    # LinkedList definition
+    def __init(self):
+        self.head = Node()
+
+    # LinkedList Operations
+    def append(self, data):
+        pass
+
+    def getByIndex(self, index):
+        pass
+
+    def traverse(self, function):
+        pass
